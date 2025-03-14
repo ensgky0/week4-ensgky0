4574783b3c19df6d76115a2131410ba8446c06ac
ensgky0 <enesgokyar@posta.mu.edu.tr>
2025-03-14 12:30:27 +0300

    Create TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,58 +1,95 @@
+ import java.util.Scanner;
 
-
-/**
- * A simple Tic-Tac-Toe game implementation in Java.
- */
 public class TicTacToe {
+    static char[][] board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
 
-	/**
-	 * The main method to run the Tic-Tac-Toe game.
-	 *
-	 * @param args Command line arguments (not used).
-	 */
-	public static void main(String[] args) {
-		// TODO: Initialize the game board and scanner
-		// TODO: Print the initial game board
-		// TODO: Prompt the current player for their move
-		// TODO: Update the board with the player's move
-		// TODO: Handle invalid move
-		// TODO: Check if the game has ended
-	}
+    static char currentPlayer = 'X';
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean gameOn = true;
 
+        while (gameOn) {
+            printBoard();
+            playerMove(scanner);
+            gameOn = checkGameStatus();
+            switchPlayer();
+        }
+        scanner.close();
+    }
 
+    static void printBoard() {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print("|");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("  -----");
+        }
+    }
 
-	/**
-	 * Checks if the current player has won the game.
-	 *
-	 * @param board The game board.
-	 * @param row The row of the last move.
-	 * @param col The column of the last move.
-	 * @return True if the current player has won, false otherwise.
-	 */
-	public static boolean checkboard(char[][] board, int row, int col) {
-		// TODO: Implement the logic to check if the current player has won
+    static void playerMove(Scanner scanner) {
+        int row, col;
+        while (true) {
+            System.out.println("Oyuncu " + currentPlayer + "'in sırası. Satır ve sütun girin (0-2): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("Geçersiz hamle! Tekrar deneyin.");
+            }
+        }
+    }
+
+    static boolean checkGameStatus() {
+        if (checkWin()) {
+            printBoard();
+            System.out.println("Oyuncu " + currentPlayer + " kazandı!");
+            return false;
+        }
+        if (isBoardFull()) {
+            printBoard();
+            System.out.println("Oyun berabere!");
+            return false;
+        }
+        return true;
+    }
+
+    static boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer)
+                return true;
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)
+                return true;
+        }
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer)
+            return true;
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)
+            return true;
         return false;
     }
 
-	/**
-	 * Prints the current state of the game board.
-	 *
-	 * @param board The game board.
-	 */
-	public static void printBoard(char[][] board) {
-		// TODO: Implement the logic to print the game board
-	}
+    static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Validates if the given row and column are within the valid range.
-	 *
-	 * @param row The row number.
-	 * @param col The column number.
-	 * @return True if the row and column are valid, false otherwise.
-	 */
-	public static boolean isValid(int row, int col) {
-		// TODO: Implement the logic to validate the row and column
-		return false;
-	}
+    static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
 }
